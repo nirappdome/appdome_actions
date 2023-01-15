8bc041225efcfebf053e336288d353319a384e3f
nirappdome <119943675+nirappdome@users.noreply.github.com>
2023-01-15 17:37:29 +0200

    Create download.sh

---
+++ actions/download.sh
@@ -0,0 +1,13 @@
+mkdir files
+VAR=$1
+if [[ $VAR != '' ]]; then
+  echo "Downloading keystore file"
+  wget $VAR -O files/file.keystore
+fi
+VAR=$2
+if [[ $VAR != '' ]]; then
+  echo "Downloading json sign overrides"
+  wget $VAR -O files/sign_overrides.json
+fi
+wget $3 -O files/vanilla.apk
+ls files
