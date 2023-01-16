1428a97e32da6b31f95b52c3fab789d0759dcbdd
nirappdome <119943675+nirappdome@users.noreply.github.com>
2023-01-16 10:15:23 +0200

    Update appdome_build_sign.sh

--- actions/appdome_build_sign.sh
+++ actions/appdome_build_sign.sh
@@ -1,39 +1,39 @@
-# $1 = ${{github.event.inputs.sign_otions}}
-# $2 = ${{github.event.inputs.sign_overrids}}
-# $3 = ${{github.event.inputs.google_play_signing}}
-# $4 = ${{github.event.inputs.fusion_set_id}}
-# $5 = ${{github.event.inputs.keystore_password}}
-# $6 = ${{github.event.inputs.keystore_alias}}
-# $7 = ${{github.event.inputs.key_pass}}
-# $8 = ${{github.event.inputs.signing_fingerprint}}
+# ${args[0]} = ${{github.event.inputs.sign_options}}
+# ${args[1]} = ${{github.event.inputs.sign_overrids}}
+# ${args[2]} = ${{github.event.inputs.google_play_signing}}
+# ${args[3]} = ${{github.event.inputs.fusion_set_id}}
+# ${args[4]} = ${{github.event.inputs.keystore_password}}
+# ${args[5]} = ${{github.event.inputs.keystore_alias}}
+# ${args[6]} = ${{github.event.inputs.key_pass}}
+# ${args[7]} = ${{github.event.inputs.signing_fingerprint}}
 
-
-CHOICE=$1
+args=("$@")
+CHOICE=${args[0]}
 if [[ $CHOICE == "Appdome Signing" ]];then
-  VAR=$2
+  VAR=${args[1]}
   if [[ -n $VAR ]]; then
-    sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --sign_on_appdome -fs $4 --keystore ./files/file.keystore --keystore_pass $5 --keystore_alias $6 --key_pass $7 --sign_overrides files/sign_overrides.json --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
+    sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --sign_on_appdome -fs ${args[3]} --keystore ./files/file.keystore --keystore_pass ${args[4]} --keystore_alias ${args[5]} --key_pass ${args[6]} --sign_overrides files/sign_overrides.json --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
   else
-    sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --sign_on_appdome -fs $4 --keystore ./files/file.keystore --keystore_pass $5 --keystore_alias $6 --key_pass $7 --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
+    sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --sign_on_appdome -fs ${args[3]} --keystore ./files/file.keystore --keystore_pass ${args[4]} --keystore_alias ${args[5]} --key_pass ${args[6]} --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
   fi
 elif [[ $CHOICE == "Private Signing" ]];then
-  VAR=$2
+  VAR=${args[1]}
   if [[ -n $VAR ]]; then
-    VAR=$3
+    VAR=${args[2]}
     if [ $VAR = true ] ; then
-      sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --private_signing -fs $4 --signing_fingerprint $8 --sign_overrides files/sign_overrides.json --google_play_signing --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
+      sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --private_signing -fs ${args[3]} --signing_fingerprint ${args[7]} --sign_overrides files/sign_overrides.json --google_play_signing --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
     else
-      sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --private_signing -fs $4 --signing_fingerprint $8 --sign_overrides files/sign_overrides.json --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
+      sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --private_signing -fs ${args[3]} --signing_fingerprint ${args[7]} --sign_overrides files/sign_overrides.json --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
     fi
   else
-    VAR=$3
+    VAR=${args[2]}
     if [ $VAR = true ] ; then
-      sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --private_signing -fs $4 --signing_fingerprint $8 --google_play_signing --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
+      sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --private_signing -fs ${args[3]} --signing_fingerprint ${args[7]} --google_play_signing --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
     else
-      sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --private_signing -fs $4 --signing_fingerprint $8 --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
+      sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --private_signing -fs ${args[3]} --signing_fingerprint ${args[7]} --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
     fi
   fi
 elif [[ $CHOICE == "Auto-Dev Private Signing" ]];then
-  sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --auto_dev_private_signing -fs $4 --signing_fingerprint $8 --google_play_signing --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
+  sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --auto_dev_private_signing -fs ${args[3]} --signing_fingerprint ${args[7]} --google_play_signing --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
 fi
 ls output
