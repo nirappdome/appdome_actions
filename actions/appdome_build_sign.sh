24d2ce4962d28752c771fbb21b412ccb7ff067b1
nirappdome <119943675+nirappdome@users.noreply.github.com>
2023-01-16 11:05:14 +0200

    Update appdome_build_sign.sh

--- actions/appdome_build_sign.sh
+++ actions/appdome_build_sign.sh
@@ -6,17 +6,17 @@
 # ${args[5]} = ${{github.event.inputs.keystore_alias}}
 # ${args[6]} = ${{github.event.inputs.key_pass}}
 # ${args[7]} = ${{github.event.inputs.signing_fingerprint}}
-
+echo "HERE!"
 args=("$@")
 CHOICE=${args[0]}
-if [[ $CHOICE == "Appdome Signing" ]];then
+if [[ $CHOICE == "Appdome_Signing" ]];then
   VAR=${args[1]}
   if [[ -n $VAR ]]; then
     sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --sign_on_appdome -fs ${args[3]} --keystore ./files/file.keystore --keystore_pass ${args[4]} --keystore_alias ${args[5]} --key_pass ${args[6]} --sign_overrides files/sign_overrides.json --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
   else
     sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --sign_on_appdome -fs ${args[3]} --keystore ./files/file.keystore --keystore_pass ${args[4]} --keystore_alias ${args[5]} --key_pass ${args[6]} --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
   fi
-elif [[ $CHOICE == "Private Signing" ]];then
+elif [[ $CHOICE == "Private_Signing" ]];then
   VAR=${args[1]}
   if [[ -n $VAR ]]; then
     VAR=${args[2]}
@@ -33,7 +33,7 @@ elif [[ $CHOICE == "Private Signing" ]];then
       sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --private_signing -fs ${args[3]} --signing_fingerprint ${args[7]} --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
     fi
   fi
-elif [[ $CHOICE == "Auto-Dev Private Signing" ]];then
+elif [[ $CHOICE == "Auto_Dev_Private_Signing" ]];then
   sudo python3 appdome/appdome-api-python/appdome_api.py -key ${{secrets.APPDOME_API_KEY}} --app ./files/vanilla.apk --auto_dev_private_signing -fs ${args[3]} --signing_fingerprint ${args[7]} --google_play_signing --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
 fi
 ls output
