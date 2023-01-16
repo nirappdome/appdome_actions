1ee355d971ba72a5501d5fcf7f58b2e43f236c36
nirappdome <119943675+nirappdome@users.noreply.github.com>
2023-01-16 11:55:30 +0200

    Update appdome_build_sign.sh

--- actions/appdome_build_sign.sh
+++ actions/appdome_build_sign.sh
@@ -11,14 +11,14 @@ args=("$@")
 CHOICE=${args[0]}
 if [[ $CHOICE == "Appdome_Signing" ]];then
   VAR=${args[1]}
-  if [[ -n $VAR ]]; then
+  if [[ $VAR != '!' ]]; then
     sudo python3 appdome/appdome-api-python/appdome_api.py -key ${args[8]} --app ./files/vanilla.apk --sign_on_appdome -fs ${args[3]} --keystore ./files/file.keystore --keystore_pass ${args[4]} --keystore_alias ${args[5]} --key_pass ${args[6]} --sign_overrides files/sign_overrides.json --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
   else
     sudo python3 appdome/appdome-api-python/appdome_api.py -key ${args[8]} --app ./files/vanilla.apk --sign_on_appdome -fs ${args[3]} --keystore ./files/file.keystore --keystore_pass ${args[4]} --keystore_alias ${args[5]} --key_pass ${args[6]} --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
   fi
 elif [[ $CHOICE == "Private_Signing" ]];then
   VAR=${args[1]}
-  if [[ -n $VAR ]]; then
+  if [[ $VAR != '!' ]]; then
     VAR=${args[2]}
     if [ $VAR = true ] ; then
       sudo python3 appdome/appdome-api-python/appdome_api.py -key ${args[8]} --app ./files/vanilla.apk --private_signing -fs ${args[3]} --signing_fingerprint ${args[7]} --sign_overrides files/sign_overrides.json --google_play_signing --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
